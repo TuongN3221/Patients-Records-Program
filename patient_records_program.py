2801d90e8e717ccd9d301ebe023b76ce58bf5603
Tuong Nguyen <yolotube21@yahoo.com>
2025-03-04 12:53:39 -0500

    Update array names

--- patient_records_program.py
+++ patient_records_program.py
@@ -1,17 +1,17 @@
-#array namesn n= name, c = condition, hrn = hospital room number, ns= night stayed, pp = patient procedure, rc= room charge, isc= insurance company, cop= co-payment
-n = ["John Doe","Jane Doe", "Bill Smith","Alena Gomez","Arnold James","Jona Johnson"]
-c = ["Broken Arm", "Stomach Laceration","Kidney Stone","Intestinal issues","Broken nose","stroke" ]
-hrn = ["328","329","214","410","407","414"]
-ns = [2, 1, 3, 4, 5, 7]
-pp = ["Surgery","Stitches","Monitored passage","Gastronomy", "Reconstructive surgery","Stint"]
-pc = [5000, 2400, 500, 1700, 14000, 400000]
-rc = [325, 325, 325, 340, 340, 340]
-insc = ["Aetna","Humana","Blue Cross","United Health Care","Cigna","Kaiser Permanente"]
-cop = [750, 0.1, 0.15, 0.12, 0.15, 500]
+# Data
+patient_name = ["John Doe","Jane Doe", "Bill Smith","Alena Gomez","Arnold James","Jona Johnson"]
+condition = ["Broken Arm", "Stomach Laceration","Kidney Stone","Intestinal issues","Broken nose","stroke" ]
+hospital_room_number = ["328","329","214","410","407","414"]
+nights_stayed = [2, 1, 3, 4, 5, 7]
+patient_proceedure = ["Surgery","Stitches","Monitored passage","Gastronomy", "Reconstructive surgery","Stint"]
+procedure_cost = [5000, 2400, 500, 1700, 14000, 400000]
+room_charge = [325, 325, 325, 340, 340, 340]
+inscurance_company = ["Aetna","Humana","Blue Cross","United Health Care","Cigna","Kaiser Permanente"]
+copayment = [750, 0.1, 0.15, 0.12, 0.15, 500]
 # idiot check for if statement thing
 z = ["1","2","3","4","5","6"]
 
-print(n,"\n",c,"\n",hrn,"\n",ns,"\n",pp,"\n",pc,"\n",rc,"\n",insc,"\n",cop,"\n",z)
+print(patient_name,"\n",condition,"\n",hospital_room_number,"\n",nights_stayed,"\n",patient_proceedure,"\n",procedure_cost,"\n",room_charge,"\n",inscurance_company,"\n",copayment,"\n",z)
 
 #start func
 def sf():
@@ -37,13 +37,13 @@ def padmit():
   if a in z:
     a = int(a)
     a = a - 1
-    n[a] = input("\nInput Patient Name: ")
-    hrn[a] = input("\nInput room number: ")
-    insc[a] = input("\nInput Insurance name: ")
-    cop[a] = input("\nInput Insurance co-payment or percentage:")
-    c[a] = input("\nInput reason for admittance: ")
-    pp[a] = input("\nInput Treatment plan: ")
-    print("\nRecord of patient", n[a], hrn[a], cop[a], c[a], pp[a])
+    patient_name[a] = input("\nInput Patient Name: ")
+    hospital_room_number[a] = input("\nInput room number: ")
+    inscurance_company[a] = input("\nInput Insurance name: ")
+    copayment[a] = input("\nInput Insurance co-payment or percentage:")
+    condition[a] = input("\nInput reason for admittance: ")
+    patient_proceedure[a] = input("\nInput Treatment plan: ")
+    print("\nRecord of patient", patient_name[a], hospital_room_number[a], copayment[a], condition[a], patient_proceedure[a])
   else:
     print("\nInvalid entry! Please eter a number 1 through 6!")
     padmit()
@@ -54,7 +54,7 @@ def pinfo():
   if a in z:
     a = int(a)
     a = a - 1
-    print ("\nRecord of patient", n[a], hrn[a], insc[a], cop[a], c[a], pp[a])
+    print ("\nRecord of patient", patient_name[a], hospital_room_number[a], inscurance_company[a], copayment[a], condition[a], patient_proceedure[a])
     sf()
   else:
     print("\nInvalid Entry! Please enter a number 1 through 6!")
@@ -65,17 +65,17 @@ def pbill():
   a = input("\nSelect a patient to view bill? Enter 1-6. ")
   if a in z:
     a = int(a)
-    a= a - 1;
-    if isinstance (cop[a], int):
-      total = ns[a] * rc[a] + pc[a]
-      instotal = total - cop[a]
+    a= a - 1
+    if isinstance (copayment[a], int):
+      total = nights_stayed[a] * room_charge[a] + procedure_cost[a]
+      instotal = total - copayment[a]
       print("\nTotal cost $"+ str(total))
       print("Total insurance pays $"+ str(instotal))
-      print("Patient total due $"+ str(cop[a]))
+      print("Patient total due $"+ str(copayment[a]))
       sf()
     else:
-      total = ns[a] * rc[a] + pc[a]
-      ptotal = cop[a] * total 
+      total = nights_stayed[a] * room_charge[a] + procedure_cost[a]
+      ptotal = copayment[a] * total 
       instotal = total - ptotal
       print("\nTotal cost $"+ str(total))
       print("Total insurance pays $"+ str(ptotal))
